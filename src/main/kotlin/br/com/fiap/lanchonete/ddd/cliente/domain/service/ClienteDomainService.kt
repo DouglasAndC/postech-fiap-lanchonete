5088122f4974de7013aef0b98d26bdebc4b90a62
mateusmosquera <MateusMosquera@gitlab.com>
2023-10-29 22:43:55 -0300

    corrigindo test do ProdutoApplicationServicetest

--- src/main/kotlin/br/com/fiap/lanchonete/ddd/cliente/domain/service/ClienteDomainService.kt
+++ src/main/kotlin/br/com/fiap/lanchonete/ddd/cliente/domain/service/ClienteDomainService.kt
@@ -2,13 +2,13 @@ package br.com.fiap.lanchonete.ddd.cliente.domain.service
 
 import br.com.fiap.lanchonete.ddd.cliente.domain.exception.ClienteExceptionEnum
 import br.com.fiap.lanchonete.ddd.cliente.domain.model.Cliente
-import br.com.fiap.lanchonete.ddd.cliente.infrastructure.repository.ClienteJPARepository
+import br.com.fiap.lanchonete.ddd.cliente.domain.repository.ClienteRepository
 import br.com.fiap.lanchonete.exception.BusinessException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class ClienteDomainService(private val clienteRepository: ClienteJPARepository) {
+class ClienteDomainService(private val clienteRepository: ClienteRepository) {
 
     @Transactional
     fun create(cliente: Cliente): Cliente {
