7879c015fc52072b934472f438a9a935636a26a5
DougAndC <sousaandradedouglas@gmail.com>
2024-01-21 19:07:36 -0300

    Ordenação por StatusPedido no getAll Pedidos

--- src/main/kotlin/br/com/fiap/lanchonete/ddd/pedido/domain/repository/PedidoRepository.kt
+++ src/main/kotlin/br/com/fiap/lanchonete/ddd/pedido/domain/repository/PedidoRepository.kt
@@ -3,10 +3,23 @@ package br.com.fiap.lanchonete.ddd.pedido.domain.repository
 import br.com.fiap.lanchonete.ddd.pedido.domain.model.Pedido
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.Query
 
 interface PedidoRepository {
     fun save(pedido: Pedido): Pedido
-    fun findAll(pageable: Pageable): Page<Pedido>
 
+    @Query("""
+            SELECT p FROM Pedido p 
+            LEFT JOIN FETCH p.cliente 
+            WHERE p.status <> 'FINALIZADO' 
+            ORDER BY 
+            CASE p.status 
+               WHEN 'PRONTO' THEN 1 
+               WHEN 'EM_PREPARACAO' THEN 2 
+               WHEN 'RECEBIDO' THEN 3 
+            END, 
+            p.status ASC
+            """)
+    fun findAll(pageable: Pageable): Page<Pedido>
     fun findPedidoById(id:Long): Pedido?
 }
