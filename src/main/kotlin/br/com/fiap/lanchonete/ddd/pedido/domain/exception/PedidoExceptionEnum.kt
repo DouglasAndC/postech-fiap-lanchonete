418dafa8432304ff6e353a34ff33e17db4d61f16
DougAndC <sousaandradedouglas@gmail.com>
2024-01-29 18:32:20 -0300

    mudanca de texto enum pedido

--- src/main/kotlin/br/com/fiap/lanchonete/ddd/pedido/domain/exception/PedidoExceptionEnum.kt
+++ src/main/kotlin/br/com/fiap/lanchonete/ddd/pedido/domain/exception/PedidoExceptionEnum.kt
@@ -9,7 +9,7 @@ enum class PedidoExceptionEnum(private val error: String,
 
     PEDIDO_NOT_FOUND("Pedido nao foi encontrado.", HttpStatus.NOT_FOUND),
     PEDIDO_STATUS_INVALID("Status do pedido é invalido para essa operacao.", HttpStatus.BAD_REQUEST),
-    PEDIDO_STATUS_PAGAMENTO_INVALID("Status do pedido pagamento do pedido é invalido para essa operacao.", HttpStatus.BAD_REQUEST);
+    PEDIDO_STATUS_PAGAMENTO_INVALID("Status pagamento do pedido é invalido para essa operacao.", HttpStatus.BAD_REQUEST);
 
     override fun getResponseError(): ResponseErrorDto {
         return ResponseErrorDto(error = error, status = httpStatusCode.value())
