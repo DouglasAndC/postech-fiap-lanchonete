771485ac953b0e207b841b607437193418ac3cc1
DougAndC <sousaandradedouglas@gmail.com>
2024-01-29 21:42:29 -0300

    increment version

--- src/main/kotlin/br/com/fiap/lanchonete/ddd/produto/domain/model/extensions/ProdutoExtensions.kt
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.lanchonete.ddd.produto.domain.model.extensions
-
-import br.com.fiap.lanchonete.ddd.produto.application.dto.request.ProdutoRequest
-import br.com.fiap.lanchonete.ddd.produto.application.dto.response.ProdutoResponse
-import br.com.fiap.lanchonete.ddd.produto.domain.model.Produto
-
-fun Produto.toDTO(): ProdutoResponse = ProdutoResponse(id = id!!,nome = nome, categoria = categoria, descricao = descricao,preco = preco, imagens = imagens)
-
-fun ProdutoRequest.toEntity(): Produto = Produto(id = null, nome = nome, categoria = categoria, descricao = descricao, preco = preco)
